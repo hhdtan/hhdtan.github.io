2bc930c51695aae4a21a638597bb33591ca3f3fa
hhdtan <52300841+hhdtan@users.noreply.github.com>
2023-06-17 18:12:25 -0230

    it's coming together.

--- typing.js
+++ typing.js
@@ -16,24 +16,27 @@
 let text = "Greetings! Welcome to my personal website.";
 
 async function typeWriter() {
-    let delay = 10;
-    typing_effect = document.getElementById("typing-effect")
-    typing_container = document.getElementById("typing-container")
-    clicky = document.getElementById("clicky")
+    let snore = 2000
+    let delay = 60;
+    typing_effect = document.getElementById("typing-effect");
+    typing_container = document.getElementById("typing-container");
+    clicky = document.getElementById("clicky");
+    ifooter = document.getElementById("ifooter");
 
     for (let i = 0; i < text.length; i++){
         typing_effect.innerHTML += text[i];
         await new Promise(r => setTimeout(r, delay));
     }
 
-    await new Promise(r => setTimeout(r, 500));
+    await new Promise(r => setTimeout(r, snore));
     
     typing_container.style.maxHeight = 0; 
-    await new Promise(r => setTimeout(r, 500));
+    await new Promise(r => setTimeout(r, snore));
     typing_container.remove()
 
     clicky.style.maxHeight = "1000px"; 
-
+    await new Promise(r => setTimeout(r, snore));
+    ifooter.style.maxHeight = "100px"
   // await new Promise(r => setTimeout(r, 2000));
 }
 
