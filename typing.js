08ea0911e7a303d402328064835e137d32c3d08a
hhdtan <52300841+hhdtan@users.noreply.github.com>
2023-06-17 12:05:18 -0230

    page structuring
    I think I like this structure, somewhat..?

--- typing.js
+++ typing.js
@@ -16,25 +16,23 @@
 let text = "Greetings! Welcome to my personal website.";
 
 async function typeWriter() {
-  let delay = 60;
-  typing_effect = document.getElementById("typing-effect")
-  typing_container = document.getElementById("typing-container")
-  aboutme = document.getElementById("aboutme")
+    let delay = 60;
+    typing_effect = document.getElementById("typing-effect")
+    typing_container = document.getElementById("typing-container")
+    clicky = document.getElementById("clicky")
 
-  for (let i = 0; i < text.length; i++){
-    typing_effect.innerHTML += text[i];
-    await new Promise(r => setTimeout(r, delay));
-  }
+    for (let i = 0; i < text.length; i++){
+        typing_effect.innerHTML += text[i];
+        await new Promise(r => setTimeout(r, delay));
+    }
 
-  await new Promise(r => setTimeout(r, 2000));
-  
-  typing_container.style.maxHeight = 0; 
-  await new Promise(r => setTimeout(r, 2000));
-  typing_container.remove()
+    await new Promise(r => setTimeout(r, 2000));
+    
+    typing_container.style.maxHeight = 0; 
+    await new Promise(r => setTimeout(r, 2000));
+    typing_container.remove()
 
-  aboutme.style.maxHeight = "1000px"; 
-
-
+    clicky.style.maxHeight = "1000px"; 
 
   // await new Promise(r => setTimeout(r, 2000));
 }
