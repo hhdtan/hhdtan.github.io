686d651a5ae95014f0bf9c93a6181dd9c4d34430
hhdtan <52300841+hhdtan@users.noreply.github.com>
2023-06-27 03:56:49 -0230

    transition animation for homepage

--- looky.js
+++ looky.js
@@ -8,50 +8,50 @@
 //     snore = 8000;
 //     console.log('sleep');
 // }
+snore = 1000;
+setTimeout(function(){
+    let eyes = document.querySelectorAll(".eye");
+    // console.log('eyes')
+    // console.log(eyes)
 
-// setTimeout(function(){},snore)
+    let eyeRect = eyes[0].getBoundingClientRect();
+    // console.log('eye rect')
+    // console.log(eyeRect)
+    let container = document.querySelector(".eyesContainer");
+    let containerRect = container.getBoundingClientRect();
 
-let eyes = document.querySelectorAll(".eye");
-// console.log('eyes')
-// console.log(eyes)
+    window.addEventListener("resize", updatePosition, false);
 
-let eyeRect = eyes[0].getBoundingClientRect();
-// console.log('eye rect')
-// console.log(eyeRect)
-let container = document.querySelector(".eyesContainer");
-let containerRect = container.getBoundingClientRect();
+    function updatePosition(e) {
+        eyeRect = eyes[0].getBoundingClientRect();
+        containerRect = container.getBoundingClientRect();
+    }
 
-window.addEventListener("resize", updatePosition, false);
+    document.body.addEventListener("mousemove", eyesFollow, false);
 
-function updatePosition(e) {
-    eyeRect = eyes[0].getBoundingClientRect();
-    containerRect = container.getBoundingClientRect();
-}
+    function eyesFollow(e) {
+        requestAnimationFrame(() => {
+        let xPos = e.pageX;
+        let yPos = e.pageY;
+        // console.log('x and y position is')
+        // console.log(xPos, yPos)
+        let xDiff = (eyeRect.x + eyeRect.width / 2) - xPos;
+        let yDiff = (eyeRect.y + eyeRect.height / 2) - yPos;
+        // console.log('xy Diff is')
+        // console.log(xDiff, yDiff)
+        let angle = (Math.atan2(yDiff, xDiff) * 180 / Math.PI) - 180;
 
-document.body.addEventListener("mousemove", eyesFollow, false);
+        container.style.setProperty("--eyeAngle", angle.toFixed(2) + "deg");
 
-function eyesFollow(e) {
-    requestAnimationFrame(() => {
-    let xPos = e.pageX;
-    let yPos = e.pageY;
-    // console.log('x and y position is')
-    // console.log(xPos, yPos)
-    let xDiff = (eyeRect.x + eyeRect.width / 2) - xPos;
-    let yDiff = (eyeRect.y + eyeRect.height / 2) - yPos;
-    // console.log('xy Diff is')
-    // console.log(xDiff, yDiff)
-    let angle = (Math.atan2(yDiff, xDiff) * 180 / Math.PI) - 180;
+        let mouseXRelativetoContainer = xPos - containerRect.x - containerRect.width / 2;
+        let mouseYRelativetoContainer = yPos - containerRect.y - containerRect.height / 2;
 
-    container.style.setProperty("--eyeAngle", angle.toFixed(2) + "deg");
+        let containerXAngle = 60 * (mouseXRelativetoContainer / window.innerWidth);
+        let containerYAngle = -1 * 60 * (mouseYRelativetoContainer / window.innerHeight);
 
-    let mouseXRelativetoContainer = xPos - containerRect.x - containerRect.width / 2;
-    let mouseYRelativetoContainer = yPos - containerRect.y - containerRect.height / 2;
+        container.style.setProperty("--xAngle", containerXAngle.toFixed(2) + "deg");
+        container.style.setProperty("--yAngle", containerYAngle.toFixed(2) + "deg");
+        });
+    }
 
-    let containerXAngle = 60 * (mouseXRelativetoContainer / window.innerWidth);
-    let containerYAngle = -1 * 60 * (mouseYRelativetoContainer / window.innerHeight);
-
-    container.style.setProperty("--xAngle", containerXAngle.toFixed(2) + "deg");
-    container.style.setProperty("--yAngle", containerYAngle.toFixed(2) + "deg");
-    });
-}
-
+},snore)
